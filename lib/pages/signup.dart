20070acedd1b3fc6bcd7f2db1583a546e8753e73
Amartya Mondal <atm1504.in@gmail.com>
2020-06-07 21:20:50 +0530

    feat: Add register page

--- lib/pages/signup.dart
+++ lib/pages/signup.dart
@@ -1,102 +1,22 @@
-import 'package:atmmart/pages/signup.dart';
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
-import 'package:fluttertoast/fluttertoast.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:atmmart/pages/login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
-import 'package:atmmart/pages/home.dart';
+import 'package:flutter/material.dart';
 
-class Login extends StatefulWidget {
+class SignUp extends StatefulWidget {
   @override
-  _LoginState createState() => _LoginState();
+  _SignUpState createState() => _SignUpState();
 }
 
-class _LoginState extends State<Login> {
-  final GoogleSignIn googleSignIn = GoogleSignIn();
+class _SignUpState extends State<SignUp> {
+  final _formKey = GlobalKey<FormState>();
   final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
   TextEditingController _emailTextController = TextEditingController();
   TextEditingController _passwordTextController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
-  SharedPreferences preferences;
+  TextEditingController _confirmPasswordTextController =
+      TextEditingController();
+  TextEditingController _nameTextController = TextEditingController();
+  String gender;
   bool loading = false;
-  bool isLoggedin = false;
-
-  @override
-  void initState() {
-    super.initState();
-    isSignedIn();
-  }
-
-  void isSignedIn() async {
-    setState(() {
-      loading = true;
-    });
-    preferences = await SharedPreferences.getInstance();
-    isLoggedin = await googleSignIn.isSignedIn();
-    if (isLoggedin) {
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => HomePage()));
-    }
-    setState(() {
-      loading = false;
-    });
-  }
-
-  Future handleSignIn() async {
-    preferences = await SharedPreferences.getInstance();
-    setState(() {
-      loading = true;
-    });
-
-    GoogleSignInAccount googleUser = await googleSignIn.signIn();
-    GoogleSignInAuthentication googleSignInAuthentication =
-        await googleUser.authentication;
-
-    final AuthCredential credential = GoogleAuthProvider.getCredential(
-      accessToken: googleSignInAuthentication.accessToken,
-      idToken: googleSignInAuthentication.idToken,
-    );
-    final AuthResult authResult =
-        await firebaseAuth.signInWithCredential(credential);
-
-    FirebaseUser firebaseUser = authResult.user;
-    if (firebaseUser != null) {
-      final QuerySnapshot result = await Firestore.instance
-          .collection("users")
-          .where("id", isEqualTo: firebaseUser.uid)
-          .getDocuments();
-      final List<DocumentSnapshot> documnents = result.documents;
-      if (documnents.length == 0) {
-        print("Inside doc  length");
-        Firestore.instance
-            .collection("users")
-            .document(firebaseUser.uid)
-            .setData({
-          "id": firebaseUser.uid,
-          "username": firebaseUser.displayName,
-          "profilePicture": firebaseUser.photoUrl,
-          "email": firebaseUser.email
-        });
-        await preferences.setString("id", firebaseUser.uid);
-        await preferences.setString("username", firebaseUser.displayName);
-        await preferences.setString("profilePicture", firebaseUser.photoUrl);
-        await preferences.setString("email", firebaseUser.email);
-      } else {
-        await preferences.setString("id", documnents[0]["id"]);
-        await preferences.setString("username", documnents[0]["username"]);
-        await preferences.setString(
-            "profilePicture", documnents[0]["profilePicture"]);
-        await preferences.setString("email", documnents[0]["email"]);
-      }
-      Fluttertoast.showToast(msg: "Successfully Logged In");
-      setState(() {
-        loading = false;
-      });
-      Navigator.pushReplacement(
-          context, MaterialPageRoute(builder: (context) => HomePage()));
-    } else {}
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -209,7 +129,7 @@ class _LoginState extends State<Login> {
                                   onPressed: () {},
                                   minWidth: MediaQuery.of(context).size.width,
                                   child: Text(
-                                    "Login",
+                                    "Register",
                                     textAlign: TextAlign.center,
                                     style: TextStyle(
                                         color: Colors.white,
@@ -225,7 +145,7 @@ class _LoginState extends State<Login> {
                                 mainAxisAlignment: MainAxisAlignment.center,
                                 children: <Widget>[
                                   Text(
-                                    "Don't have an Account? Click here to",
+                                    "Already have an Account? Click here to",
                                     style: TextStyle(
                                         color: Colors.white, fontSize: 16),
                                   ),
@@ -236,11 +156,10 @@ class _LoginState extends State<Login> {
                                         Navigator.pushReplacement(
                                             context,
                                             MaterialPageRoute(
-                                                builder: (context) =>
-                                                    SignUp()));
+                                                builder: (context) => Login()));
                                       },
                                       child: Text(
-                                        "Register!",
+                                        "Login!",
                                         style: TextStyle(
                                             color: Colors.red,
                                             fontSize: 20,
@@ -273,7 +192,7 @@ class _LoginState extends State<Login> {
                                 elevation: 0.2,
                                 child: MaterialButton(
                                   onPressed: () {
-                                    handleSignIn();
+//                                    handleSignIn();
                                   },
                                   minWidth: MediaQuery.of(context).size.width,
                                   child: Row(
